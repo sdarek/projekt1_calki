18a19007a53654733d199488c65aaa2204ba3a6e
sdarek <darek.surdel@gmail.com>
2022-03-08 21:43:25 +0100

    szkielet

---
+++ calka.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include "calki.c"
+
+double f1(double x)
+{
+    return 0.23*x*x*x+2*x-99;            
+}     
+double f2(double x)
+{
+    double y;
+    y=sin(2*x)+cos(3*x)-1;
+    return y;       
+}
+
+
+int main(int argc, char *argv[])
+{
+    
+    printf("od=");
+    scanf("%lf",&c_od);
+    printf("do=");
+    scanf("%lf",&c_do); 
+    printf ("\n\nFunkcja 1 \n");
+    printf( "wynik prostokaty - %lf\n" ,prostokaty(f1));
+    printf( "wynik trapezy    - %lf\n" ,trapezy(f1));
+    printf( "wynik MC   - %lf\n", mc(f1));
+    printf ("\n\nFunkcja 2 \n");
+    printf( "wynik prostokaty - %lf\n" ,prostokaty(f2));
+    printf( "wynik trapezy    - %lf\n" ,trapezy(f2));
+    printf("wynik MC   - %lf\n", mc(f2));
+    printf ("\n\nFunkcja 3 \n");
+	printf( "wynik prostokaty - %lf\n" ,prostokaty(f3));
+    printf( "wynik trapezy    - %lf\n" ,trapezy(f3));
+    printf( "wynik MC   - %lf\n", mc(f3));
+    printf ("\n\nFunkcja 4 \n");
+	printf( "wynik prostokaty - %lf\n" ,prostokaty(f4));
+    printf( "wynik trapezy    - %lf\n" ,trapezy(f4));
+    printf( "wynik MC   - %lf\n", mc(f4));
+    
+    system("PAUSE");
+    return 0;
+}
